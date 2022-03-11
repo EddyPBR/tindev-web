67e2d159ac898fbcc1e01ed243a194b3efe21ede
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-10 22:05:33 -0300

    feat: create nextjs app with tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <Head>
+        <title>Home | Tindev</title>
+        <meta
+          name="description"
+          content="Aplicação para localização de desenvolvedores próximos geograficamente"
+        />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+
+      <div className="h-screen w-screen bg-gray-900 text-red-500 flex flex-col items-center justify-center gap-2">
+        <h1 className="text-4xl text-gray-100">TINDEV</h1>
+        <p className="text-sm text-gray-200">By: EddyPBR</p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
