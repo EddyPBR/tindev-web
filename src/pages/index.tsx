8239f7a9d928939353f7a6422105b48884babe7a
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-10 23:08:01 -0300

    feat: add new page 'index'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,32 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Home | Tindev</title>
+        <title>Tindev</title>
         <meta name="description" content="Aplicação para localização de desenvolvedores próximos geograficamente" />
       </Head>
 
-      <div className="h-screen w-screen bg-gray-900 text-red-500 flex flex-col items-center justify-center gap-2">
-        <h1 className="text-4xl text-gray-100 font-cursive">Tindev</h1>
-        <p className="text-sm text-gray-200 font-sans">By: EddyPBR</p>
+      <div className="h-screen w-screen bg-gray-100 flex flex-col items-center justify-center">
+        <main className="flex flex-col w-full max-w-xs gap-8">
+          <Image src="/assets/logo.svg" alt="Tindev" priority layout="responsive" width={300} height={106} />
+          <form className="flex flex-col gap-4">
+            <input
+              type="text"
+              placeholder="Digite seu usuário no Github"
+              className="h-12 w-full px-5 border border-gray-200 rounded text-gray-600"
+            />
+            <button
+              type="submit"
+              className="bg-primary text-gray-100 font-bold font-cursive h-12 rounded hover:bg-orange-700 hover:text-white transition-colors shadow-sm"
+            >
+              Enviar
+            </button>
+          </form>
+        </main>
       </div>
     </>
   );
