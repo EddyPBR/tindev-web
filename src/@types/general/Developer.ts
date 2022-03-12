4f656204e7f9bd9d891ccdb015d13d670d366598
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-11 23:45:31 -0300

    feat: add new type Developer

---
+++ src/@types/general/Developer.ts
@@ -0,0 +1,11 @@
+interface Developer {
+  id: string;
+  name: string;
+  user: string;
+  bio: string;
+  avatar_url: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export type { Developer };
