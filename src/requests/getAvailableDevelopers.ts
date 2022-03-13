7445fe7730a4f8eab124ce60c834f2265a38fa1b
EddyPBR <edvaldojunior1310@gmail.com>
2022-03-13 11:09:16 -0300

    refactor: request getAvailableDevelopers now return the request function

--- src/requests/getAvailableDevelopers.ts
+++ src/requests/getAvailableDevelopers.ts
@@ -1,19 +1,12 @@
 import { api } from "@services/api";
 import type { GetAvailableDevelopers } from "@@types/requests";
 import type { AvailableDeveloper } from "@@types/developer";
-import type { AxiosError } from "axios";
 
-const getAvailableDevelopers = async ({ id }: GetAvailableDevelopers) => {
-  try {
-    return await api.get<AvailableDeveloper[]>("/developers", {
-      headers: {
-        id,
-      },
-    });
-  } catch (error) {
-    const axiosError = error as AxiosError<{ message: string }, Error>;
-    throw axiosError.response?.data?.message as string;
-  }
-};
+const getAvailableDevelopers = async ({ id }: GetAvailableDevelopers) =>
+  await api.get<AvailableDeveloper[]>("/developers", {
+    headers: {
+      id,
+    },
+  });
 
 export { getAvailableDevelopers };
